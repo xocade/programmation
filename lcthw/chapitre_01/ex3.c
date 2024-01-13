c9270a453dba9bde5fbb95c45ff21f9d3f3027ff
Julius Cesar <mail@xocade.com>
2024-01-13 16:22:00 +0400

    Added 3rd exercise.

---
+++ lcthw/chapitre_01/ex3.c
@@ -0,0 +1,14 @@
+/* Lern C The Hard Way: Exercise 3
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	int birthYear = 1988;
+	int currentYear = 2024;
+
+	printf("You were born on %d.\n", birthYear);
+	printf("You must be %d years old.\n", currentYear - birthYear);
+
+	return 0;
+}
