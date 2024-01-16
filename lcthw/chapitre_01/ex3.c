c264b93d319163bfb033abc92377429d4eda3acf
Julius C. Cesar <mail@xocade.com>
2024-01-16 17:16:34 +0400

    Added exercise 7.

--- lcthw/chapitre_01/ex3.c
+++ lcthw/chapitre_01/ex3.c
@@ -1,8 +1,22 @@
-/* Lern C The Hard Way: Exercise 3
+/* Learn C The Hard Way: Exercise 3
 */
 
 #include <stdio.h>
 
+int main()
+{
+	int age = 10;
+	int height = 72;
+
+	printf("I am %d years old.\n", age);
+	printf("I am %d inches tall. \n", height);
+
+	return 0;
+}
+
+
+
+/*
 int main(void) {
 	int birthYear = 1988;
 	int currentYear = 2024;
@@ -12,3 +26,6 @@ int main(void) {
 
 	return 0;
 }
+*/
+
+
